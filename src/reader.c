bbc02e86b00dd4b3ffefab157093ec955cd99ea9
saqttra <113034629+saqttra@users.noreply.github.com>
2025-11-03 21:37:25 -0600

    feat [v 0.6.2-alpha] master: --scan cmd v.1

--- src/reader.c
+++ src/reader.c
@@ -51,21 +51,22 @@ void free_reader(Reader** readerPtr)
   *readerPtr = NULL;
 }
 
-/* static void print_header(const unsigned long digits); */
+static void print_header(const unsigned long digits);
 static size_t refill(Reader* reader);
 
 void scan(Reader* reader)
 {
-  /* const int digits = count_digits(ULONG_MAX); */
+  const int digits = count_digits(ULONG_MAX);
   Byte runeBuf[UTF8_MAX_WIDTH] = {0};
   int runeWidth = 0, i;
+  int hexbuf;
   Rune codepoint;
 
-  /* TODO: for later use */
-  /* size_t offset, line, col;
-    offset = line = col = 0; */
+  /*size_t offset, line, col;
+  offset = line = col = 0;*/
+
   /* Line Col Bytes UTF-8 Unicode Rune */
-  /* print_header(digits); */
+  print_header(digits);
 
   while(1){
     if(reader->offset >= reader->bytesRead){
@@ -84,27 +85,55 @@ void scan(Reader* reader)
 
     for(i=0; i < runeWidth; i++)
       runeBuf[i] = reader->readBuf[reader->offset + i];
-
     codepoint = decode_seq(runeBuf);
-    printf("U+%04lX : %ld\n", codepoint, codepoint);
+
+    printf("%-5d ", runeWidth);
+
+    hexbuf = 0;
+    for(i=0; i < runeWidth; i++)
+      hexbuf += printf("0x%02X ", runeBuf[i]);
+
+    for (; hexbuf < 20; hexbuf++)
+      putchar(' ');
+
+    printf("U+%04lX : %s\n", codepoint, runeBuf);
+
+    /*printf(
+      "%0*lu %*lu %*lu %-5d ",
+      digits, offset,
+      -digits, line,
+      -digits, col,
+      runeWidth
+    );*/
 
     reader->offset += runeWidth;
+
+    /* clean buffer */
+    runeBuf[0] = runeBuf[1] = runeBuf[2] = runeBuf[3] = 0;
   }
 }
 
-/* static void print_header(const unsigned long digits)
+static void print_header(const unsigned long digits)
 {
-  int offsetWidth = digits > strlen("Offset") ? digits : strlen("Offset");
-  int lineWidth   = digits > strlen("Line")   ? digits : strlen("Line");
-  int colWidth    = digits > strlen("Col")    ? digits : strlen("Col");
+  /* Offset Line Col Width UTF-8 Unicode/Rune */
+
+  /* temp to silence compiler warnings */
+  digits > strlen("Offset") ? digits : strlen("Offset");
+  /* int thWidth  = digits > strlen("Offset") ? digits : strlen("Offset");
+     int th2Width = digits > strlen("Line")   ? digits : strlen("Line");
+     int th3Width = digits > strlen("Col")    ? digits : strlen("Col");
+  
+     -thWidth, "Offset",
+     -th2Width, "Line",
+     -th3Width, "Col", */
 
   printf(
     "%*s %*s %*s\n", 
-    -offsetWidth, "Offset", 
-    -lineWidth, "Line", 
-    -colWidth, "Col"
+    -(int)strlen("Width"), "Width",
+    -19, "UTF-8",
+    -8, "Unicode/Rune"
   );
-} */
+}
 
 static size_t refill(Reader* reader)
 {
@@ -134,18 +163,3 @@ static size_t refill(Reader* reader)
 }
 
 #undef UTF8_MAX_WIDTH
-
-/* TODO: for later use */
-/*for(i = 0; i < reader->bytesRead; i++){
-  printf(
-    "%0*lu %*lu %*lu\n",
-    digits, offset,
-    -digits, line,
-    -digits, col
-  );
-  printf("%X ", reader->readBuf[i]);
-
-  col++;
-  offset++;
-  reader->offset++;
-}*/
